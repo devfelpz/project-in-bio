e23038ee8f8810992cf82425f629964fb4619c5b
Felipe Araujo <fela.19silva@gmail.com>
2025-03-05 21:00:57 -0300

    finalizado configuração do authjs

--- app/components/landing-page/header.tsx
+++ app/components/landing-page/header.tsx
@@ -1,6 +1,11 @@
+import { manageAuth } from '@/app/actions/manage-auth';
+import { auth } from '@/app/lib/auth';
 import Button from '../ui/button';
 
-const Header = () => {
+const Header = async () => {
+	const session = await auth();
+
+	console.log(session);
 	return (
 		<div className="absoulute top-0 left-0 right-0 max-w-7xl mx-auto flex items-center justify-between py-10">
 			<div className="flex items-center gap-4">
@@ -8,8 +13,10 @@ const Header = () => {
 				<h3 className="text-white text-2xl font-bold">ProjectInBio</h3>
 			</div>
 			<div className="flex items-center gap-3">
-				<Button>Minha Página</Button>
-				<Button>Sair</Button>
+				{session && <Button>Minha Página</Button>}
+				<form action={manageAuth}>
+					<Button>{session ? 'Sair' : 'Login'}</Button>
+				</form>
 			</div>
 		</div>
 	);
--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -3,17 +3,16 @@ import { getFirestore } from 'firebase-admin/firestore';
 import { getStorage } from 'firebase-admin/storage';
 import 'server-only';
 
-const decodedKey = Buffer.from(
-	// biome-ignore lint/style/noNonNullAssertion: <explanation>
-	process.env.FIREBASE_PRIVATE_KEY!,
-	'base64',
-).toString('utf8');
+const privateKeyFormatted = (process.env.FIREBASE_PRIVATE_KEY || '').replace(
+	/\\n/g,
+	'\n',
+);
 
 // Certificado
 export const firebaseCert = cert({
 	projectId: process.env.FIREBASE_PROJECT_ID,
 	clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-	privateKey: decodedKey,
+	privateKey: privateKeyFormatted,
 });
 
 //Instacia do app
